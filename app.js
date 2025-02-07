0853da020c7d7705aec4795993b0e79d0618fdbe
May Donoban <rppride_2411@hotmail.com>
2025-02-07 11:04:29 -0500

    Add env

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const usersRoutes = require('./routes/users.js');
-const cardsRoutes = require('./routes/cards.js')
+const cardsRoutes = require('./routes/cards.js');
+require('dotenv').config()
 
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/mydb').then(() => { //direccion IP requerible
+const DB_AROUND = process.env.DB_AROUND || 'arounddb';
+mongoose.connect(`mongodb://127.0.0.1:27017/${DB_AROUND}`).then(() => { //direccion IP requerible
   console.log('Connected to MongoDB');
 })
   .catch((err) => {
